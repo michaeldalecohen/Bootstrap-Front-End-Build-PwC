d2fce199de3bec5f5e1154cd2ae6e34e0065a5b1
michaeldalecohen <michaeldalecohen@gmail.com>
2015-04-27 15:10:17 +1000

    Revert 07abab4..cd699a1
    This rolls back to commit 07abab43abbb4e9230c416f26e266bb9f115d1c1.

--- js/functions.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll( function() {
-	var windowScroll = $(this).scrollTop();
-
-	$('header > #zoom').css({
-    'top': '-' + windowScroll + 'px',
-    'right': '-' + windowScroll + 'px',
-    'left': '-' + windowScroll + 'px'
-  });
-});
